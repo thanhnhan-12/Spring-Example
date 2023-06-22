c026f3880a2f6022cba49a90e4e57fa49cf8e93f
unknown <nguyenthanhnhan12701@gmail.com>
2023-06-22 17:53:54 +0700

    Spring Example

---
+++ src/main/java/com/springboot/Main.java
@@ -0,0 +1,25 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "com.springboot")
+//@EnableAutoConfiguration
+//@Configuration
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello";
+    }
+}
