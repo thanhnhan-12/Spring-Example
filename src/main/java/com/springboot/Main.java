d21905ecfbf1f9bdef7e266a233b51fb980d42e7
unknown <nguyenthanhnhan12701@gmail.com>
2023-06-22 22:28:09 +0700

    Java Objects to JSON Objects

--- src/main/java/com/springboot/Main.java
+++ src/main/java/com/springboot/Main.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 //@ComponentScan(basePackages = "com.springboot")
 //@EnableAutoConfiguration
@@ -19,7 +21,51 @@ public class Main {
     }
 
     @GetMapping("/greet")
-    public String greet() {
-        return "Hello";
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse(
+                "Hello", List.of("Java", "Golang", "Javascript"), new Person("Alex", 22, 30_000)
+        );
+
+        return response;
     }
+
+    record Person(String name, int age, double savings) {
+
+    }
+
+    record GreetResponse(String greet, List<String> favProgramming, Person person) {
+    }
+
+//    class GreetResponse {
+//        private final String greet;
+//
+//        GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(greet);
+//        }
+//    }
+
 }
